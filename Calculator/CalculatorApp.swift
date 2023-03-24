64d70e677977a5c4a7f1b8dfd9ed2ab2349dd09a
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-03-24 13:36:44 +0100

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Hossein Sharifi on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
